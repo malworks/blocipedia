e96345148d5146b31e2db4f254d81424e765b4ad
Mallory <malloryworks@gmail.com>
2017-02-01 18:44:47 -0800

    Checkpoint 2:  Sign up Progress

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -33,6 +33,8 @@ Rails.application.configure do
 
   # for devise gem
   config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
+  config.action_mailer.delivery_method = :smtp
+
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
---
+++ db/migrate/20170129060308_add_confirmable_to_devise_v1.rb
@@ -0,0 +1,4 @@
+class AddConfirmableToDeviseV1 < ActiveRecord::Migration[5.0]
+  def change
+  end
+end
