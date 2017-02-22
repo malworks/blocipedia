abdf4ce9cbf8857e5c3a314633294edd782fc89e
Mallory <malloryworks@gmail.com>
2017-02-21 21:11:59 -0800

    Checkpoint 4: Completed U & D. Finished checkpoint.

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -27,5 +27,34 @@ class WikisController < ApplicationController
   end
 
   def edit
+    @wiki = Wiki.find(params[:id])
   end
+
+  def update
+    @wiki = Wiki.find(params[:id])
+    @wiki.title = params[:wiki][:title]
+    @wiki.body = params[:wiki][:body]
+    @wiki.user_id = current_user.id
+
+    if @wiki.save
+      flash[:notice] = "Wiki was updated."
+      redirect_to @wiki
+    else
+      flash.now[:alert] = "There was an error saving the wiki. Please try again."
+      render :edit
+    end
+  end
+
+  def destroy
+    @wiki = Wiki.find(params[:id])
+    @wiki.user_id = current_user.id
+
+    if @wiki.destroy
+      flash[:notice] = "\"#{@wiki.title}\" was deleted successfully."
+       redirect_to @wiki
+     else
+       flash.now[:alert] = "There was an error deleting the wiki."
+       render :show
+     end
+   end
 end
