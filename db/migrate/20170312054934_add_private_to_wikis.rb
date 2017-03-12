4fcd4d42c75c8fe5609b21e42a4df8bc80d52361
Mallory <malloryworks@gmail.com>
2017-03-11 22:16:01 -0800

    Checkpoint 8: Functionality works but private boolean not changed in db

---
+++ db/migrate/20170312054934_add_private_to_wikis.rb
@@ -0,0 +1,5 @@
+class AddPrivateToWikis < ActiveRecord::Migration[5.0]
+  #def change
+    #add_column :wikis, :private, :boolean, default: false
+  #end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170224020345) do
+ActiveRecord::Schema.define(version: 20170312054934) do
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -35,8 +35,8 @@ ActiveRecord::Schema.define(version: 20170224020345) do
 
   create_table "wikis", force: :cascade do |t|
     t.string   "title"
-    t.string   "body"
-    t.boolean  "private" default: false
+    t.text     "body"
+    t.boolean  "private"
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
