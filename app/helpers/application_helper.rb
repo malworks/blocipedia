e5350251ec3b8a80618194f1b43f991b705759a3
Mallory <malloryworks@gmail.com>
2017-03-11 22:51:59 -0800

    Checkpoint 9: Markdown

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,17 @@
 module ApplicationHelper
+  def markdown(text)
+        renderer = Redcarpet::Render::HTML.new(hard_wrap: true, filter_html: true)
+        options = {
+            fenced_code_blocks: true,
+            no_intra_emphasis: true,
+            autolink: true,
+            strikethrough: true,
+            lax_html_blocks: true,
+            superscript: true,
+            tables: true,
+            underline: true,
+            highlight: true
+        }
+        Redcarpet::Markdown.new(renderer, options).render(text).html_safe
+    end
 end
